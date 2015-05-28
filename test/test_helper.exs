a92b466ac9e4c9cde80dc9b39bf2b87fc5603654
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-05-28 15:39:03 +0200

    Run tests on 2.4

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,12 @@
 ExUnit.start()
 
 {_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.dropDatabase()')
-System.cmd("mongo", ~w'mongodb_test --eval db.createUser({user:"test_user",pwd:"test_user",roles:[]})')
+{_, 0} = System.cmd("mongo", ~w'mongodb_test2 --eval db.dropDatabase()')
+
+# 2.4
+{_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.addUser({user:"mongodb_user",pwd:"mongodb_user",roles:[]})')
+{_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.addUser({user:"mongodb_user2",pwd:"mongodb_user2",roles:[]})')
+
+# >2.6
+# {_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.createUser({user:"mongodb_user",pwd:"mongodb_user",roles:[]})')
+# {_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.createUser({user:"mongodb_user2",pwd:"mongodb_user2",roles:[]})')
