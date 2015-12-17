8cbefe47c8f75c33d95d67a35ab9674962932243
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-12-17 15:48:47 +0100

    Bump to v0.1.2-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mongodb.Mixfile do
 
   def project do
     [app: :mongodb,
-     version: "0.1.1",
+     version: "0.1.2-dev",
      elixir: "~> 1.0",
      deps: deps,
      name: "Mongodb",
