4f0500e2df0f44ce72352f5de1b6fe5c717031e2
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-04 01:35:23 +0200

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -1,17 +1,15 @@
 defmodule Mongodb.Mixfile do
   use Mix.Project
 
+  @version "0.1.2-dev"
+
   def project do
     [app: :mongodb,
-     version: "0.1.2-dev",
+     version: @version,
      elixir: "~> 1.0",
-     deps: deps,
      name: "Mongodb",
-     source_url: "https://github.com/ericmj/mongodb",
-     docs: fn ->
-       {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-       [source_ref: ref, main: "readme", extras: ["README.md"]]
-     end,
+     deps: deps,
+     docs: docs,
      description: description,
      package: package]
   end
@@ -25,18 +23,24 @@ defmodule Mongodb.Mixfile do
   defp deps do
     [{:connection,    "~> 1.0"},
      {:db_connection, "~> 0.2.5"},
-     {:poolboy,       "~> 1.5", optional: true},
      {:ex_doc,        ">= 0.0.0", only: :docs},
      {:earmark,       ">= 0.0.0", only: :docs}]
   end
 
+  defp docs do
+    [main: "readme",
+     extras: ["README.md"],
+     source_ref: "v#{@version}",
+     source_url: "https://github.com/ericmj/mongodb"]
+  end
+
   defp description do
-    "MongoDB driver for Elixir."
+    "MongoDB driver for Elixir"
   end
 
   defp package do
     [maintainers: ["Eric Meadows-Jönsson"],
      licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/ericmj/mongodb"}]
+     links: %{"GitHub" => "https://github.com/ericmj/mongodb"}]
   end
 end
