34daeb182ce6c0067b7e75b0766c8a2d8d4e2a2b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-04 01:32:24 +0200

    Move app callback to Mongo.App

---
+++ lib/mongo/app.ex
@@ -0,0 +1,15 @@
+defmodule Mongo.App do
+  @moduledoc false
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Mongo.IdServer, []),
+      worker(Mongo.PBKDF2Cache, []),
+    ]
+
+    opts = [strategy: :one_for_one, name: Mongo.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
