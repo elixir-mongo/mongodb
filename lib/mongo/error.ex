08921a9df55bc7479daad5af02aff360e5d358a1
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-04 01:35:09 +0200

    Handle tcp errors in Mongo.Error

--- lib/mongo/error.ex
+++ lib/mongo/error.ex
@@ -1,4 +1,21 @@
 defmodule Mongo.Error do
-  @type t :: %Mongo.Error{__exception__: true, message: binary, code: integer}
   defexception [:message, :code]
+
+  def message(e) do
+    code = if e.code, do: " " <> e.code
+    "#{e.message}#{code}"
+  end
+
+  def exception(tag: :tcp, action: action, reason: reason) do
+    formatted_reason = :inet.format_error(reason)
+    %Mongo.Error{message: "tcp #{action}: #{formatted_reason} - #{inspect(reason)}"}
+  end
+
+  def exception(message: message, code: code) do
+    %Mongo.Error{message: message, code: code}
+  end
+
+  def exception(message: message) do
+    %Mongo.Error{message: message}
+  end
 end
