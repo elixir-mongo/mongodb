372f25d9b7049b118f141d210d8a0fdf39ad7708
Justin Wood <justin.k.wood@gmail.com>
2017-04-05 21:31:55 -0400

    drop databases and output Mongo version on test runs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,11 @@ if System.get_env("CI") do
 end
 
 ExUnit.start()
+
+{string, 0} = System.cmd("mongod", ~w'--version')
+["db version v" <> version, _] = String.split(string, "\n", parts: 2)
+
+IO.puts "[mongod v#{version}]"
+
+{_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.dropDatabase() --port 27001')
+{_, 0} = System.cmd("mongo", ~w'mongodb_test2 --eval db.dropDatabase() --port 27001')
