f427db5848b62a44699fbe76ce7fcfae423ab99e
Matías Reyes <matreyes@gmail.com>
2017-07-13 16:40:53 -0400

    Fixes to PR #133: - Default mongodb port: 27001 - Commented instead of removing an alias

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -10,8 +10,8 @@ ExUnit.start()
 
 IO.puts "[mongod v#{version}]"
 
-{_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.dropDatabase() --port 27017')
-{_, 0} = System.cmd("mongo", ~w'mongodb_test2 --eval db.dropDatabase() --port 27017')
+{_, 0} = System.cmd("mongo", ~w'mongodb_test --eval db.dropDatabase() --port 27001')
+{_, 0} = System.cmd("mongo", ~w'mongodb_test2 --eval db.dropDatabase() --port 27001')
 
 version =
   version
