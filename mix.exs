f110d73962cb43dfb5145b4fbd2b51bba11522eb
sam_lin <sam@swingcloud.app>
2018-11-08 16:32:16 +0800

    ⬆️ update db_connection

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Mongodb.Mixfile do
   defp deps do
     [
       {:connection,    "~> 1.0"},
-      {:db_connection, "~> 1.1"},
+      {:db_connection, "~> 2.0"},
       {:decimal,       "~> 1.0"},
       {:poolboy,       ">= 0.0.0", only: :test},
       {:jason,         "~> 1.0.0", only: :test},
