92142ca465f88252cfacae82f98a010880f7b231
Hakan B <hbarisik@yahoo.com>
2019-12-03 14:36:44 +0300

    Used Keyword list instead of Map

--- lib/mongo/config_hide.ex
+++ lib/mongo/config_hide.ex
@@ -2,52 +2,39 @@ defmodule Mongo.ConfigHide do
   @default_user    "anonymous"
   @password_masked "***"
   
-  def to_options_with_password_masked_if_defined(original_opts) do
-    case Map.get(original_opts, :password) do
+  def to_options_with_password_masked_if_defined(original_opts_as_keyword_list) do
+    case Keyword.get(original_opts_as_keyword_list, :password) do
       nil ->  
-        original_opts   
+        original_opts_as_keyword_list   
       actual_password  -> 
-        original_opts
+        original_opts_as_keyword_list
         |>retain_password_in_env(actual_password)
 
-        original_opts
-        |>Map.replace!(:password, @password_masked)
+        original_opts_as_keyword_list
+        |>Keyword.replace!(:password, @password_masked)
     end
   end
  
   def to_options_list_with_actual_password_if_defined(opts_as_keyword_list) do 
-    opts= 
-      opts_as_keyword_list
-        |> Enum.into(%{}) 
-
-    updated_opts=
-      case Map.get(opts, :password) do
-        nil ->  
-          opts   
-        received_password -> 
-          case (@password_masked==received_password) do
-            true  -> 
-              actual_password=retreive_password(opts_as_keyword_list)
-              Map.replace!(opts, :password, actual_password )
-            false -> 
-              opts        
-          end  
-      end   
-
-    updated_opts
-    |> Enum.to_list() 
+    case Keyword.get(opts_as_keyword_list, :password) do
+      nil ->  
+        opts_as_keyword_list   
+      received_password -> 
+        case (@password_masked==received_password) do
+          true  -> 
+            actual_password=retreive_password(opts_as_keyword_list)
+            Keyword.replace!(opts_as_keyword_list, :password, actual_password )
+          false -> 
+              opts_as_keyword_list        
+        end  
+    end   
   end 
 
-  defp retreive_password(opts_as_keylist) do 
-    opts=
-      opts_as_keylist
-      |> Enum.into(%{})  
-    {user, hostname, port}=user_hostname_port(opts)
+  defp retreive_password(opts_as_keyword_list) do 
+    {user, hostname, port}=user_hostname_port(opts_as_keyword_list)
     retreive_password(user, hostname, port)
   end 
 
-  #  
-  
   defp retain_password_in_env(opts, password) do 
     {user, hostname, port}=user_hostname_port(opts)
     retain_password_in_env(user, hostname, port, password )
@@ -76,10 +63,10 @@ defmodule Mongo.ConfigHide do
     "PASSWORD_FOR_" <>  user <> "_AT_" <>  String.replace( hostname, ".", "_")  <> "_ON_"  <>  port_str
   end
 
-  defp user_hostname_port(opts) do  
-    user=Map.get(opts, :user, @default_user )
-    hostname=Map.fetch!(opts, :hostname)
-    port=Map.fetch!(opts, :port)
+  defp user_hostname_port(opts_keyword_list) do  
+    user=Keyword.get(opts_keyword_list, :user, @default_user )
+    hostname=Keyword.fetch!(opts_keyword_list, :hostname)
+    port=Keyword.fetch!(opts_keyword_list, :port)
     {user, hostname, port}
   end 
 
