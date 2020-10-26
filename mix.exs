5448837c9862985fee1927feef6bd9279cc7b2be
Łukasz Jan Niemier <lukasz@niemier.pl>
2020-10-26 13:14:58 +0100

    fix: typo in the mix.exs

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Mongodb.Mixfile do
     [
       mod: {Mongo.App, []},
       env: [],
-      extra_applications: [:crytpo, :logger, :ssl],
+      extra_applications: [:crypto, :logger, :ssl],
       registered: [
         Mongo.PBKDF2Cache,
         Mongo.Session.Supervisor,
