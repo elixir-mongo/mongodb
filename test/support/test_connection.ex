2f6fb957cd5c7b23d88485a0351cbccfa6f46f0d
Joe Pearson <joe.pearson@avid.com>
2021-09-28 10:38:26 -0700

    Attempt to run tests in CI

--- test/support/test_connection.ex
+++ test/support/test_connection.ex
@@ -1,14 +1,14 @@
 defmodule Mongo.TestConnection do
-  @seeds ["127.0.0.1:27001", "127.0.0.1:27002", "127.0.0.1:27003"]
+  @seeds ["127.0.0.1:27017", "127.0.0.1:27018", "127.0.0.1:27019"]
 
   import ExUnit.Callbacks, only: [start_supervised: 1]
 
   def connect() do
-    with {_, 0} <- System.cmd("bash", ["./start_mongo.bash"]) do
+    # with {_, 0} <- System.cmd("bash", ["./start_mongo.bash"]) do
       start_supervised({Mongo, database: "mongodb_test", seeds: @seeds})
-    else
-      {error, exit_code} ->
-        {:error, {exit_code, error}}
-    end
+    # else
+    #   {error, exit_code} ->
+    #     {:error, {exit_code, error}}
+    # end
   end
 end
