0c30773015ac727ec1f98d949817e57e774ae236
Joe Pearson <joe.pearson@avid.com>
2021-09-28 18:41:12 -0700

    Split jobs

--- test/support/test_connection.ex
+++ test/support/test_connection.ex
@@ -1,6 +1,5 @@
 defmodule Mongo.TestConnection do
-  @seeds ["127.0.0.1:27017"]
-  # @seeds ["127.0.0.1:27017"]
+  @seeds ["127.0.0.1:27017", "127.0.0.1:27018", "127.0.0.1:27019"]
 
   import ExUnit.Callbacks, only: [start_supervised: 1]
 
